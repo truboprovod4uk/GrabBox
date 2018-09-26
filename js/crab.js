c479e1af7c9049d356f789f2f85fb849385ccbe4
truboprovod4uk <truboprovod4uk@gmail.com>
2018-09-26 09:21:26 +0300

    third commit

--- js/crab.js
+++ js/crab.js
@@ -5,45 +5,54 @@ document.addEventListener("DOMContentLoaded",
         var h=Number((getComputedStyle(document.getElementById("playground")).height).replace("px",""));
         var w=Number((getComputedStyle(document.getElementById("playground")).width).replace("px",""));
 
-        function crabPos(){
-            var crab = new Object()
-            crab.marginLeftOfCrab=Number((getComputedStyle(document.getElementById("crab")).marginLeft).replace("px",""));
-            crab.marginTopOfCrab=Number((getComputedStyle(document.getElementById("crab")).marginTop).replace("px",""));
-            return crab;
+        function cbPos(){
+            var crabBoxPos = new Object()
+            crabBoxPos.marginLeftOfCrab=Number((getComputedStyle(document.getElementById("crab")).marginLeft).replace("px",""));
+            crabBoxPos.marginTopOfCrab=Number((getComputedStyle(document.getElementById("crab")).marginTop).replace("px",""));
+            crabBoxPos.marginLeftOfBox=Number((getComputedStyle(document.getElementById("box")).marginLeft).replace("px",""));
+            crabBoxPos.marginTopOfBox=Number((getComputedStyle(document.getElementById("box")).marginTop).replace("px",""));
+            crabBoxPos.deltaMarginLeft=function(){
+               return (crabBoxPos.marginLeftOfCrab-crabBoxPos.marginLeftOfBox-12.5)/50
+               console.log((crabBoxPos.marginLeftOfCrab-crabBoxPos.marginLeftOfBox-12.5)/50);
+            }
+            crabBoxPos.deltaMarginTop=function(){
+               return (crabBoxPos.marginTopOfCrab-crabBoxPos.marginTopOfBox-12.5)/50
+               console.log((crabBoxPos.marginTopOfCrab-crabBoxPos.marginTopOfBox-12.5)/50);
+            }
+            return crabBoxPos;
         }
 
         function right(){
-        	var c = crabPos();
+        	var c = cbPos();
         	if ((c.marginLeftOfCrab+delta)<w){
         		document.getElementById("crab").style.marginLeft=c.marginLeftOfCrab+delta+"px";
         	}
         }
 
         function left(){
-            var c = crabPos();
+            var c = cbPos();
         	if(c.marginLeftOfCrab>delta){
         		document.getElementById("crab").style.marginLeft=c.marginLeftOfCrab-delta+"px";
         	}
         }
 
         function down(){
-        	var c = crabPos();
+        	var c = cbPos();
         	if ((c.marginTopOfCrab+delta)<h){
         		document.getElementById("crab").style.marginTop=c.marginTopOfCrab+delta+"px";
         	}
         }
 
         function up(){
-            var c = crabPos();
+            var c = cbPos();
             if(c.marginTopOfCrab>delta){
             	document.getElementById("crab").style.marginTop=c.marginTopOfCrab-delta+"px";
             }
         }
 
         function runX(){
-            var marginLeftOfCrab=Number((getComputedStyle(document.getElementById("crab")).marginLeft).replace("px",""));
-            var marginLeftOfBox=Number((getComputedStyle(document.getElementById("box")).marginLeft).replace("px",""));
-            var deltaMarginLeft=(marginLeftOfCrab-marginLeftOfBox-12.5)/50;
+            var cb=cbPos();
+            deltaMarginLeft=cb.deltaMarginLeft();
             if (deltaMarginLeft<0){
                 var timerId=setInterval(right,1000);
                 setTimeout(function(){clearInterval(timerId)}, Math.abs(1000*deltaMarginLeft));
@@ -54,9 +63,8 @@ document.addEventListener("DOMContentLoaded",
 
         }
         function runY(){
-            var marginTopOfBox=Number((getComputedStyle(document.getElementById("box")).marginTop).replace("px",""));
-            var marginTopOfCrab=Number((getComputedStyle(document.getElementById("crab")).marginTop).replace("px",""));
-            var deltaMarginTop=(marginTopOfCrab-marginTopOfBox-12.5)/50;
+            var cb=cbPos();
+            deltaMarginTop=cb.deltaMarginTop();
             if (deltaMarginTop<0){
                 var timerId=setInterval(down,1000);
                 setTimeout(function(){clearInterval(timerId)}, Math.abs(1000*deltaMarginTop));
@@ -66,6 +74,32 @@ document.addEventListener("DOMContentLoaded",
             }
 
         }
+
+
+        /*function runX(){
+            var cb=cbPos();
+            deltaMarginLeft=cb.deltaMarginLeft();
+            if (deltaMarginLeft<0){
+                var timerId=setInterval(right,1000);
+                console.log(deltaMarginLeft);
+                setTimeout(function(){clearInterval(timerId)}, Math.abs(1000*deltaMarginLeft));
+            }else if(deltaMarginLeft>0){
+                var timerId=setInterval(left,1000);
+                setTimeout(function(){clearInterval(timerId)}, Math.abs(1000*deltaMarginLeft));
+            }
+            var cb=cbPos()
+            deltaMarginLeft=cb.deltaMarginLeft();
+            console.log(deltaMarginLeft);
+            deltaMarginTop=cb.deltaMarginTop();
+            if (deltaMarginTop<0&&deltaMarginLeft==0){
+                var timerId=setInterval(down,1000);
+                setTimeout(function(){clearInterval(timerId)}, Math.abs(1000*deltaMarginTop));
+            }else if(deltaMarginTop>0){
+                var timerId=setInterval(up,1000);
+                setTimeout(function(){clearInterval(timerId)}, Math.abs(1000*deltaMarginTop));
+            }
+        }*/
+
 
 
 
